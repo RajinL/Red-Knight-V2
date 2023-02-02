83b7ac3c2bfd626b79d5b810b74dc81aeccd94b3
RajinL <rajin.lalgee@hotmail.com>
2023-02-02 17:27:02 -0500

    Basic sticky platform

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -13,8 +13,8 @@ public class PlayerController : MonoBehaviour
     private Rigidbody2D playerRigidbody = null;
     public float movementSpeed = 4.0f;
 
-    [SerializeField]
-    private float jumpPower = 9.0f;
+    //[SerializeField]
+    //private float jumpPower = 9.0f;
 
     public bool grounded
     {
