2efd8c387aeb8504d770d27d6c3f5f3cb1dd4167
AJ Scrivner <aj.hortobagyi@gmail.com>
2023-02-03 16:27:56 +0000

    Outside Scene added
    With SceneSwitch trigger plus code,
    Old PlayerControl and some initial animation setup

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Animator anim;
+
+    private SpriteRenderer sprite;
+    /*private float dirX = 0f;*/
+
+    [SerializeField]
+    private float jumpPower = 9f;
+    /*private float jumpPower = 7f;*/
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Search for this component once during start instead of searching every time you want to use it
+        rb = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float dirX = Input.GetAxisRaw("Horizontal");
+
+        rb.velocity = new Vector2(dirX * 7f, rb.velocity.y);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            anim.SetBool("jumping", true);
+
+            rb.velocity = new Vector2(rb.velocity.x, jumpPower);
+
+        }
+
+        /*UpdateAnimationUpdate();*/
+
+        if (dirX > 0f)
+        {
+            anim.SetBool("running", true);
+            sprite.flipX = false;
+        }
+        else if (dirX < 0f)
+        {
+            anim.SetBool("running", true);
+            sprite.flipX = true;
+        }
+        else
+        {
+            anim.SetBool("running", false);
+        }
+    }
+
+    
+
+    /*void UpdateAnimationUpdate()
+    {
+        if (dirX > 0f)
+        {
+            anim.SetBool("running", true);
+            sprite.flipX = false;
+        }
+        else if (dirX < 0f)
+        {
+            anim.SetBool("running", true);
+            sprite.flipX = true;
+        }
+        else
+        {
+            anim.SetBool("running", false);
+        }
+    }*/
+}
