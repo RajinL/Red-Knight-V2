a723503a92e780b06a8d533f326044968ea2e21d
RajinL <rajin.lalgee@hotmail.com>
2023-02-03 11:52:18 -0500

    Very basic, broken shooting

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -34,7 +34,7 @@ public class PlayerMovement : MonoBehaviour
     private void Update()
     {
         dirX = Input.GetAxisRaw("Horizontal");
-        Debug.Log(dirX);
+        //Debug.Log(dirX);
 
         rb.velocity = new Vector2(dirX * playerSpeed, rb.velocity.y);
 
