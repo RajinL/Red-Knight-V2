12931717e9464951a7c45f75f87453025a5676de
RajinL <rajin.lalgee@hotmail.com>
2023-02-04 19:43:42 -0500

    Created barrel, crate, and garlic bomb prefabs

---
+++ Assets/Scripts/Combat/GarlicBomb.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GarlicBomb : MonoBehaviour
+{
+    public Transform bombDrop;
+    public GameObject bombPrefab;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire2"))
+        {
+            DropBomb();
+        }
+    }
+
+    void DropBomb()
+    {
+        Instantiate(bombPrefab, bombDrop.position, bombDrop.rotation);
+    }
+}
