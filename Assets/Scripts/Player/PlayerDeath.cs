daf827016e391745bebd107e2fefa3444d8c6bdc
AJ Scrivner <aj.hortobagyi@gmail.com>
2023-02-06 15:21:16 +0000

    SpikeCollider prefab added (Player will play Dead animation upon collision)

---
+++ Assets/Scripts/Player/PlayerDeath.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+
+public class PlayerDeath : MonoBehaviour
+{
+    [Header("Custom Event")]
+    public UnityEvent myEvents;
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if (collision.gameObject.name == "Player")
+        {
+            if (myEvents == null)
+            {
+
+            }
+            else
+            {
+                myEvents.Invoke();
+            }
+        }
+        
+    }
+}
