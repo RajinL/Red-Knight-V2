43607aac33cb64314b7df3dfee2488857ec3b5d9
AJ Scrivner <aj.hortobagyi@gmail.com>
2023-02-06 14:58:50 +0000

    Built Level 1 Part 3, and created chest prefab (opens when colliding with player)

---
+++ Assets/Scripts/Objects/OpenChest.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OpenChest : MonoBehaviour
+{
+    public Sprite ChestOpened;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        //StartCoroutine(ExecuteAfterTime(1));
+        //StartCoroutine(ExecuteAfterTime(0));
+        this.gameObject.GetComponent<SpriteRenderer>().sprite = ChestOpened;
+
+    }
+}
