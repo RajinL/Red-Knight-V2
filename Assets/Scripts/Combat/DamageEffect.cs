6ce267823d3f455ee7cb8facbbfc9237e0a628fb
RajinL <rajin.lalgee@hotmail.com>
2023-02-07 18:22:02 -0500

    Added damage effect to enemy and objects

---
+++ Assets/Scripts/Combat/DamageEffect.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageEffect : MonoBehaviour
+{
+    [SerializeField] private Material damageMaterial;
+    [SerializeField] private float duration;
+
+    private SpriteRenderer spriteRenderer;
+
+    private Material originalMaterial;
+
+    private Coroutine damageRoutine;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        originalMaterial = spriteRenderer.material;
+    }
+
+    // Update is called once per frame
+    public void Damage()
+    {
+        if (damageRoutine != null)
+        {
+            StopCoroutine(damageRoutine);
+        }
+
+        damageRoutine = StartCoroutine(DamageRoutine());
+    }
+
+    private IEnumerator DamageRoutine()
+    {
+
+        spriteRenderer.material = damageMaterial;
+
+        yield return new WaitForSeconds(duration);
+
+        spriteRenderer.material = originalMaterial;
+
+        damageRoutine = null;
+    }
+}
