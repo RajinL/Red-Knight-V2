63cf23d42f4bb1ae34306c9c21243171b03d0451
RajinL <rajin.lalgee@hotmail.com>
2023-02-11 17:33:08 -0500

    Barrel and crate destruction animation

---
+++ Assets/Scripts/Objects/DestructibleObject.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestructibleObject : MonoBehaviour
+{
+    [SerializeField]
+    Vector2 forceDirection;
+
+    [SerializeField]
+    int torque;
+
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        float randomTorque = Random.Range(-30, 30);
+        float randomForceX = Random.Range(forceDirection.x - 30, forceDirection.x + 30);
+        float randomForceY = Random.Range(forceDirection.y, forceDirection.y + 30);
+
+        forceDirection.x = randomForceX;
+        forceDirection.y = randomForceY;
+
+        rb = GetComponent<Rigidbody2D>();
+        rb.AddForce(forceDirection);
+        rb.AddTorque(randomTorque);
+    }
+}
