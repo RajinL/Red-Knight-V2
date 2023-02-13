de960bd83eecfdca2f67c37f1331d832ed978f78
AJ Scrivner <aj.hortobagyi@gmail.com>
2023-02-13 18:04:55 +0000

    Key needs to be collected to open door on Level1part3
    CollectKey script created, SceneSwitch2 script created (that checks if key has been collected), key object has been created

---
+++ Assets/Scripts/Managers/SceneSwitch2.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitch2 : MonoBehaviour
+{
+    /*void OnTriggerEnter(Collider other)
+    {
+        SceneManager.LoadScene(1);
+    }*/
+
+    public CollectKey checkKey;
+
+    [SerializeField]
+    public int sceneNumber;
+
+
+    //if we want the scene to load with a delay
+    IEnumerator ExecuteAfterTime(float time)
+    {
+        yield return new WaitForSeconds(time);
+
+        // Code to execute after the delay
+        SceneManager.LoadScene(1);
+
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            if (collision.tag == "Player")
+            {
+                if (checkKey.hasKey)
+                {
+                    SceneManager.LoadScene(sceneNumber);
+                }
+                else
+                {
+                    //code here for alert box
+                    //"oh no, the door is seems to be locked!"
+                }
+
+            }
+        }
+        //StartCoroutine(ExecuteAfterTime(1));
+        //StartCoroutine(ExecuteAfterTime(0));
+
+    }
+}
