fa5ef869c56d6bb81d32c89103b329aa6254d2e6
Tim Little <timlittle88@gmail.com>
2023-02-18 15:01:46 +0000

    Add the start of the boss fight, with simple AI

---
+++ Assets/Scripts/Boss/Boss.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss : MonoBehaviour
+{
+
+    public Transform player;
+
+    public bool isFlipped = false;
+
+    public void LookAtPlayer()
+    {
+
+        if (transform.position.x > player.position.x && isFlipped)
+        {
+            Flip();
+        }
+        else if (transform.position.x < player.position.x && !isFlipped)
+        {
+            Flip();
+        }
+    }
+
+    private void Flip()
+    {
+        Vector3 flipped = transform.localScale;
+        flipped.z *= -1f;
+        transform.localScale = flipped;
+        transform.Rotate(0f, 180f, 0f);
+        isFlipped = !isFlipped;
+
+    }
+
+}
