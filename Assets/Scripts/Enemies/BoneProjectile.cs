7d91f1a0d3936157ba68c57e2886ef5cbd16aadd
RajinL <rajin.lalgee@hotmail.com>
2023-02-23 13:59:53 -0500

    SkeletonAlt shoots projectile at player

--- Assets/Scripts/Enemies/BoneProjectile.cs
+++ Assets/Scripts/Enemies/BoneProjectile.cs
@@ -4,19 +4,46 @@ using UnityEngine;
 
 public class BoneProjectile : MonoBehaviour
 {
-    public float speed;
-    public float rotationSpeed;
+    private GameObject player;
     private Rigidbody2D rb;
+    public float force;
+    public float speed = 10f;
+    public float rotationSpeed = 10f;
+    [SerializeField] private int attackDamage = 1;
 
     // Start is called before the first frame update
     void Start()
-    {
-        rb.velocity = transform.right * speed;
+    {       
+        rb = GetComponent<Rigidbody2D>();
+        player = GameObject.FindGameObjectWithTag("Player");
+
+        //Vector3 direction = player.transform.position - transform.position;
+        //rb.velocity = new Vector2(direction.x, direction.y).normalized * force; 
+
+        //rb.velocity = new Vector2(direction.x, direction.y) * speed;
+
+        if (player.transform.position.x < this.transform.position.x)
+        {
+            rb.velocity = -transform.right * speed;
+        }
+        else
+        {
+            rb.velocity = transform.right * speed;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+        if (hitInfo.gameObject.tag == "Player")
+        {
+            hitInfo.gameObject.GetComponent<Health>().TakeDamage(attackDamage);
+            Destroy(gameObject);
+        }
     }
 }
---
+++ Assets/Scripts/Enemies/SkeletonAlt.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkeletonAlt : MonoBehaviour
+{
+    private GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player.transform.position.x < this.transform.position.x)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        else
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+    }
+}
