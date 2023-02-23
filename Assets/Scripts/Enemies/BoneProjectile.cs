c9397977865b02b2c7831dd464b36b2c572fe6f9
RajinL <rajin.lalgee@hotmail.com>
2023-02-22 23:02:24 -0500

    Working on skeleton bone projectile

---
+++ Assets/Scripts/Enemies/BoneProjectile.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoneProjectile : MonoBehaviour
+{
+    public float speed;
+    public float rotationSpeed;
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.velocity = transform.right * speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
