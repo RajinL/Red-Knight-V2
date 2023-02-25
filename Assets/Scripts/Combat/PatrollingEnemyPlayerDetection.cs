81bf77aad6a50e9912dff1356d341137f6160a18
RajinL <rajin.lalgee@hotmail.com>
2023-02-25 16:15:45 -0500

    Can shoot past dead enemy skeleton

---
+++ Assets/Scripts/Combat/PatrollingEnemyPlayerDetection.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PatrollingEnemyPlayerDetection : MonoBehaviour
+{
+    public LayerMask targetLayer;
+
+    [SerializeField]
+    public UnityEvent<GameObject> OnPlayerDetected;
+
+    [Range(0.1f, 1)]
+    public float radius;
+
+    [Header("Gizmo Parameters")]
+    public Color gizmoColor = Color.green;
+    public bool showGizmos = true;
+
+    public bool PlayerDetected { get; internal set; }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var collider = Physics2D.OverlapCircle(transform.position, radius, targetLayer);
+        PlayerDetected = collider != null;
+        if (PlayerDetected)
+        {
+            OnPlayerDetected?.Invoke(collider.gameObject);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (showGizmos)
+        {
+            Gizmos.color = gizmoColor;
+            Gizmos.DrawSphere(transform.position, radius);
+        }
+    }
+}
