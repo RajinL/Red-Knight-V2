5b890b641256cf763642ea014774c99f2eb63041
RajinL <rajin.lalgee@hotmail.com>
2023-02-26 17:25:32 -0500

    Vampire shoots bomb projectile

---
+++ Assets/Scripts/Enemies/BombProjectile.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombProjectile : BombDamage
+{
+    private GameObject player;
+    private Rigidbody2D rb;
+
+    //[SerializeField] private float speed = 10f;
+
+    // Start is called before the first frame update
+    public override void InitializeBomb()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        player = GameObject.FindGameObjectWithTag("Player");
+
+        if (player.transform.position.x < this.transform.position.x)
+        {
+            
+            rb.velocity = -transform.right * speedThrown;
+            
+
+        }
+        else
+        {
+            
+            rb.velocity = transform.right * speedThrown;
+            
+        }
+        detonationTime = initialDetTime;
+        totalTimeToTurnOff = detonationTime + timeToTurnOffAfterDet;
+    }
+}
---
+++ Assets/Scripts/Enemies/VampireShoot.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VampireShoot : MonoBehaviour
+{
+    [field: SerializeField]
+    public bool PlayerDetected { get; private set; }
+
+    [Header("OverlapBox Params")]
+    [SerializeField]
+    private Transform detectorOrigin;
+    public Vector2 detectorSize = Vector2.one;
+    public Vector2 detectorOriginOffset = Vector2.zero;
+
+    public float detectionDelay = 1.5f;
+
+    public LayerMask detectorLayerMask;
+
+    [Header("Gizmo Params")]
+    public Color gizmoIdleColor = Color.green;
+    public Color gizmoDetectedColor = Color.red;
+    public bool showGizmos = true;
+
+    private GameObject target;
+
+    [Header("Bullet Params")]
+    public GameObject bomb;
+    public Transform bombPos;
+
+
+
+    public GameObject Target
+    {
+        get => target;
+        private set
+        {
+            target = value;
+            PlayerDetected = target != null;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(DetectionCoroutine());
+    }
+
+    IEnumerator DetectionCoroutine()
+    {
+        yield return new WaitForSeconds(detectionDelay);
+        PerformDetection();
+        StartCoroutine(DetectionCoroutine());
+    }
+
+    public void PerformDetection()
+    {
+        Collider2D collider = Physics2D.OverlapBox((Vector2)detectorOrigin.position + detectorOriginOffset, detectorSize, 0, detectorLayerMask);
+        if (collider != null)
+        {
+            Target = collider.gameObject;
+            Shoot();
+        }
+        else
+        {
+            Target = null;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (showGizmos && detectorOrigin != null)
+        {
+            Gizmos.color = gizmoIdleColor;
+            if (PlayerDetected)
+            {
+                Gizmos.color = gizmoDetectedColor;
+            }
+            Gizmos.DrawCube((Vector2)detectorOrigin.position + detectorOriginOffset, detectorSize);
+        }
+    }
+
+    void Shoot()
+    {
+        Instantiate(bomb, bombPos.position, Quaternion.identity);
+
+    }
+}
