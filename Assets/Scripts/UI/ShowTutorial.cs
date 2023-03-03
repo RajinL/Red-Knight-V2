202ca3a93f01b32f2a586487142203ba85ad1006
RajinL <rajin.lalgee@hotmail.com>
2023-03-03 01:05:52 -0500

    create simple show tutorial on outside level

---
+++ Assets/Scripts/UI/ShowTutorial.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowTutorial : MonoBehaviour
+{
+
+    public GameObject tutorialObject; 
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            tutorialObject.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            tutorialObject.SetActive(false);
+        }
+    }
+}
