2466425e2343c706c53e383d3826551ea80aec73
SamSherrod <107281526+SamSherrod@users.noreply.github.com>
2023-03-03 15:39:45 -0800

    Make garlic bombs appear out of chest, and increment bombs by one if player's not already at the max bomb limit. Changed bomb limit to 25. Organized several scripts in project folder.

---
+++ Assets/Scripts/Objects/GarBombPickup.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This class inherits from the Pickup class and will give the player extra bombs when picked up
+/// </summary>
+public class GarBombPickup : Pickup
+{
+    [Header("Bomb Info")]
+    [Tooltip("The amount of bombs that will be given to the player upon collision")]
+    [SerializeField] private int amountOfBombs;
+
+    public override void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject == GameManager.instance.player)
+        {
+            AddBomb(1);
+        }
+        base.OnTriggerEnter2D(collision);
+    }
+
+    /// <summary>
+    /// Adds an amount of bomb ammo and updates the UI. If the player is already at max bombs,
+    /// the currentGarlicBombCount will not be incremented.
+    /// </summary>
+    /// <param name="bombCount">The amount of bombs to add.</param>
+    private void AddBomb(int bombCount)
+    {
+        GameManager.CurrentGarlicBombCount += bombCount;
+        if (GameManager.CurrentGarlicBombCount > GameManager.MaxGarlicBombCount)
+        {
+            GameManager.CurrentGarlicBombCount = GameManager.MaxGarlicBombCount;
+        }
+        GameManager.instance.UpdateUI();
+    }
+}
---
+++ Assets/Scripts/Objects/Pickup.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    public virtual void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject == GameManager.instance.player)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
